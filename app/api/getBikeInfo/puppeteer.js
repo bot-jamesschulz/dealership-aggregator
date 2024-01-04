89ccd3b8436b8f8f8451e2f9d43d30e0ac9282e3
bot-jamesschulz <42590220+bot-jamesschulz@users.noreply.github.com>
2024-01-03 16:40:38 -0800

    Refactor searchForMatchingAnchors to use css selectors instead of xpath

--- app/api/getBikeInfo/puppeteer.js
+++ app/api/getBikeInfo/puppeteer.js
@@ -1,49 +1,60 @@
-const puppeteer = require('puppeteer-extra')
+const puppeteer = require('puppeteer-extra');
 
 // add stealth plugin and use defaults (all evasion techniques)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 
 // Search for anchor content that includes one of the make from vehicleMakes
-async function vehicleTitleAnchorSearch(page,vehicleMakes) {
+async function searchForMatchingAnchors(page,vehicleMakes) {
   let vehicleInfo = {};
 
-  console.log("Searching for anchor titles on:", page.url())
-  for (const make of vehicleMakes) {
-    const xpath = `//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), "${make}")]`;
-    const elementHandles = await page.$x(xpath);
-    // Map through elementHandles that contain the make i.e. 'yamaha', and return the text content and href
-    const makeElementInfoList = await Promise.all(elementHandles.map(async elementHandle => await page.evaluate(element => ({"content": element.textContent,"href": element.getAttribute("href")}), elementHandle)));
+  // Get all anchor elements
+  const anchorHandles = await page.$$('a');
+
+  // Get the content/attributes from the anchors
+  const anchorData = await Promise.all(anchorHandles.map(async (anchorHandle) => {
+    const innerText = await anchorHandle.evaluate(node => node.innerText);
+    const href = await anchorHandle.evaluate(node => node.getAttribute("href"));
     
-    makeElementInfoList.forEach(({ content, href }) => {
+    return { innerText, href };
+  }));
+ 
+  console.log("Searching for anchor titles on:", page.url())
+  // Look for through anchorData for innerText that includes the make
+  for (const make of vehicleMakes) {
+    
+    // Look through anchors that include the make and check if they are valid listings
+    anchorData.forEach(({ innerText, href }) => {
+      const trimmedText = innerText.trim().split('\n')[0];
       
-      // Look for a substring of 4 digits
+      if (!innerText || !trimmedText.toLowerCase().includes(make)) {
+        return;
+      };
+
+      // Look substrings of 4 digits
       const yearPattern = /(\d{4})/g;
-      const matches = content.match(yearPattern);
+      const matches = trimmedText.match(yearPattern);
       const lowerBound = 1950;
       const upperBound = 2025;
-      let validMatches;
-      // Check if the digit substring is in a year range
-      if (matches) {
-        validMatches = matches.filter(match => {
+      
+      // Check if any of the digit substrings are in the correct range
+      const validMatches = matches?.find(match => {
           const year = parseInt(match);
           return year >= lowerBound && year <= upperBound;
         });
-      }
 
-      let validUrl;
+      // If we have a valid title then add it to vehicleInfo
       if (validMatches) {
-        // Clean string
-        const trimmedContent = content.trim().split('\n')[0];
-        validUrl = getNewUrl(href, page);
+        const validUrl = getNewUrl(href, page);
         if (vehicleInfo[make]) {
-          vehicleInfo[make].push({"title": trimmedContent, "url": validUrl});
+          vehicleInfo[make].push({"title": trimmedText, "url": validUrl});
         } else {
-          vehicleInfo[make] = [{"title": trimmedContent, "url": validUrl}];
+          vehicleInfo[make] = [{"title": trimmedText, "url": validUrl}];
         }
       } 
     });
   }
+  
   return vehicleInfo;
 }
 
@@ -136,7 +147,7 @@ async function sortedAnchorHrefs(page,searchTexts,anchorContentSearch) {
 
 async function goToNewTab(url, browser) {
   const page = await browser.newPage();
-  await page.goto(url,{ waitUntil: 'networkidle2' });
+  await page.goto(url,{ waitUntil: 'networkidle0' });
   return page;
 }
 
@@ -182,19 +193,71 @@ async function getInventoryPages (url, browser) {
   }
 }
 
-async function getListings(page) {
-  console.log("getting listings on:", page.url());
-  const makes = ['benelli', 'beta', 'bmw', 'can-am', 'ducati', 'greenger', 'hisun', 'honda', 'husqvarna', 'indian', 'karavan', 'kawasaki', 'ktm', 'kymco', 'mv agusta', 'polaris', 'ssr', 'stacyc', 'suzuki', 'triumph', 'yamaha'];
+async function getNextPage(page) {
   try {
-    const listings = await vehicleTitleAnchorSearch(page,makes);
-    console.log(`Listings for: ${page.url()}`);
-    logNestedObject(listings);
+    console.log("Clicking next");
+    let xpath = `//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), "next")]`;
+    
+    let elementHandles = await page.$x(xpath);
+    if (elementHandles.length > 0) {
+      console.log("clicking anchor");
+      await elementHandles[0].click();
+      console.log("clicked anchor")
+      await page.waitForNetworkIdle({ idleTime: 250 });
 
+      return page;
+    } 
+
+    xpath = `//span[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), "next")]`;
+    elementHandles = await page.$x(xpath);
+    if (elementHandles.length > 0) {
+      console.log("clicking span");
+      await elementHandles[0].click();
+      console.log("clicked span")
+      await page.waitForNetworkIdle({ idleTime: 250 });
+
+      return page;
+    }
+
+    xpath = `//*[@aria-label[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), "next")]]`;
+    elementHandles = await page.$x(xpath);
+    if (elementHandles.length > 0) {
+      console.log("clicking aria-label");
+      await elementHandles[0].click();
+      console.log("clicked aria-label");
+      await page.waitForNetworkIdle({ idleTime: 250 });
+      console.log("network is idle");
+      return page;
+    }
+    return null;
+    console.log("Next page loaded");
+    
   } catch (error) {
     console.error('Error:', error);
-  } finally {
-    await page.close();
   }
+}
+
+async function getListings(page) {
+  console.log("getting listings on:", page.url());
+  const makes = ['benelli', 'beta', 'bmw', 'can-am', 'ducati', 'greenger', 'harley',  'hisun', 'honda', 'husqvarna', 'indian', 'karavan', 'kawasaki', 'ktm', 'kayo', 'kymco', 'moke', 'mv agusta', 'polaris', 'ssr', 'stacyc', 'suzuki', 'triumph', 'yamaha'];
+  try {
+    const listings = await searchForMatchingAnchors(page, makes);
+    console.log(`Listings for: ${page.url()}`);
+    logNestedObject(listings);
+    const currentPageContent = await page.content();
+    
+    const nextPage = await getNextPage(page);
+    console.log("Next page loaded")
+    const nextPageContent = await nextPage.content();
+
+    if (currentPageContent != nextPageContent) {
+      await getListings(nextPage);
+    }
+    
+    return 
+  } catch (error) {
+    console.error('Error:', error);
+  } 
 }
 
 export default async function getInfo(urls) {
@@ -208,7 +271,9 @@ export default async function getInfo(urls) {
       
       for (const  [inventoryType,page] of inventoryPages){
         console.log(`Getting '${inventoryType}' listings for ${page.url()}`)
+        await page.bringToFront();
         await getListings(page);
+        await page.close();
       }
 
       allSitesHrefs.push(inventoryPages);
